aaa04865b8efd573d464fd15f5e80c496170b29a
OmarMohamed74 <74471544+OmarMohamed74@users.noreply.github.com>
2022-06-15 15:18:41 +0200

    Time: 165 ms (69.70%), Space: 53.8 MB (20.61%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.cs
@@ -0,0 +1,29 @@
+public class Solution {
+         public IList<string> FizzBuzz(int n)
+        {
+            List<string> list = new List<string>();
+
+            for(int i = 1; i < n+1; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    list.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0 )
+                {
+
+                    list.Add("Fizz");
+                }
+                else if (i % 5 == 0) 
+                {
+                    list.Add("Buzz");
+                }
+                else
+                {
+                    list.Add(i.ToString());
+                }
+            }
+
+            return list;
+        }
+}
