38f76dfceab45ab06331d82dbf5d4a17f41e3168
OmarMohamed74 <74471544+OmarMohamed74@users.noreply.github.com>
2022-06-19 22:55:35 +0200

    Time: 102 ms (75.05%), Space: 41.8 MB (6.13%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MostWordsFound(string[] sentences) {
+      int max = 0;
+    for(int i = 0; i < sentences.Length; i++)
+    { 
+        string[] words = sentences[i].Split(' ');
+        if(words.Length > max)
+            max = words.Length;
+    }
+    return max;
+    }
+}
