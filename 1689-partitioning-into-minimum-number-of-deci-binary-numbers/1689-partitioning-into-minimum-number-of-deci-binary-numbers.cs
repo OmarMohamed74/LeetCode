2f7fb50a5d973c51bdd01cae465224734e230e59
OmarMohamed74 <74471544+OmarMohamed74@users.noreply.github.com>
2022-06-20 00:28:27 +0200

    Time: 126 ms (36.79%), Space: 40.2 MB (55.66%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cs
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cs
@@ -1,10 +1,9 @@
 public class Solution {
     public int MinPartitions(string n) {
         
-        int max = 0;
-    for(int i = 0; i < n.Length; i++)
-        if (n[i] - '0' > max)  // asciicode
-            max = n[i] - '0';
+    int max = 0;
+   for (int i = 0; i < n.Length; i++)
+     max = Math.Max(n[i] - '0' , max);
         
         return max;
     }
