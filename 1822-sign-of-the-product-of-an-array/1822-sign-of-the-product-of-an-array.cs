19bfe2275e36b1f386ad61e87d04706e6153c7ef
OmarMohamed74 <74471544+OmarMohamed74@users.noreply.github.com>
2022-08-07 22:24:53 +0200

    Time: 112 ms (70.44%), Space: 37.9 MB (30.29%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int ArraySign(int[] nums) {
+        var result=1;
+        foreach(var num in nums){
+            if (num==0){
+                
+                return 0;
+            } else {
+               result *=num > 0 ? 1 : -1;
+            }
+            
+        }
+        
+        return result;
+        
+       
+        
+    }
+}
